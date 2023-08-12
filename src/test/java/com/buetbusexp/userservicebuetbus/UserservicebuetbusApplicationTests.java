b2848165c006b94d6d33f92ae23daf048703d614
emran-mostofa <1505048.em@ugrad.cse.buet.ac.bd>
2023-08-12 21:21:33 +0600

    first commit

---
+++ src/test/java/com/buetbusexp/userservicebuetbus/UserservicebuetbusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buetbusexp.userservicebuetbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserservicebuetbusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
