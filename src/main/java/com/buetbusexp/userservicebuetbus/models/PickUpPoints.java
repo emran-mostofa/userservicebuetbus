2a83616cca1e531ee20e316a790edef129bdd03f
emran-mostofa <1505048.em@ugrad.cse.buet.ac.bd>
2023-08-20 13:42:40 +0600

    testing the tracking on deployed db again

--- src/main/java/com/buetbusexp/userservicebuetbus/models/PickUpPoints.java
+++ src/main/java/com/buetbusexp/userservicebuetbus/models/PickUpPoints.java
@@ -1,26 +1,26 @@
-package com.buetbusexp.userservicebuetbus.models;
-
-
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.Setter;
-
-@Entity
-@Table(name = "pickuppoints")
-@Getter
-@Setter
-public class PickUpPoints {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private  Long id;
-
-    @ManyToOne
-    @JoinColumn(name = "route_id")
-    private Route route;
-
-}
-
+//package com.buetbusexp.userservicebuetbus.models;
+//
+//
+//import jakarta.persistence.*;
+//import lombok.AllArgsConstructor;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.Id;
+//import jakarta.persistence.Table;
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//@Entity
+//@Table(name = "pickuppoints")
+//@Getter
+//@Setter
+//public class PickUpPoints {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private  Long id;
+//
+//    @ManyToOne
+//    @JoinColumn(name = "route_id")
+//    private Route route;
+//
+//}
+//
