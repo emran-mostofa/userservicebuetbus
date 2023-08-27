f8ec056be8cdbf8da1e099480b44028b9102f0e9
emran-mostofa <1505048.em@ugrad.cse.buet.ac.bd>
2023-08-27 07:54:52 +0600

    bus fields added

--- src/test/java/com/buetbusexp/userservicebuetbus/UserservicebuetbusApplicationTests.java
+++ src/test/java/com/buetbusexp/userservicebuetbus/UserservicebuetbusApplicationTests.java
@@ -9,8 +9,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 class UserservicebuetbusApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//
+//	}
 
 }
